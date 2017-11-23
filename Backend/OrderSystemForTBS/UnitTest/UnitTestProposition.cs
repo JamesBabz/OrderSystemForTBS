849e01247d2e12c3060ad1ee3474c42d6baf9c7b
JamesBabz <web.fuhlendorff@gmail.com>
2017-11-23 16:08:27 +0100

    test
    unit tests

--- Backend/OrderSystemForTBS/UnitTest/UnitTestProposition.cs
+++ Backend/OrderSystemForTBS/UnitTest/UnitTestProposition.cs
@@ -1,4 +1,5 @@
-﻿using BLL;
+﻿using System;
+using BLL;
 using BLL.BusinessObjects;
 using BLL.Facade;
 using BLL.Services;
@@ -15,13 +16,14 @@ namespace UnitTest
     [TestClass]
     public class UnitTestProposition
     {
-        [TestMethod]
-        public void CreateMethod()
+
+        public IService<PropositionBO> getMockService()
         {
 
             //Creates an InMemoryDatabaseContext for use in the UOW
             var c = new OrderSystemContext(new DbContextOptionsBuilder<OrderSystemContext>()
                 .UseInMemoryDatabase("Database").Options);
+            c.Database.EnsureDeleted();
 
             //Creates a mock DALFacade and initiates it with the InMemoeryDatabaseContext
             Mock<IDALFacade> dalFacadeMock = new Mock<IDALFacade>();
@@ -29,14 +31,61 @@ namespace UnitTest
 
             //Creates the Service
             IService<PropositionBO> service = new PropositionService(dalFacadeMock.Object);
+            return service;
+        }
 
+        [TestMethod]
+        public void CreatePropositionMethod()
+        {
+            var service = getMockService();
 
-            var snurf = service.Create(new PropositionBO() { Title = "ost" });
+            var snurf = service.Create(new PropositionBO()
+            {
+                Id = 1,
+                Title = "ost",
+                Description = "beskrivelse",
+                CreationDate = DateTime.MinValue,
+                EmployeeId = 2,
+                FileId = 3,
+                CustomerId = 4
+            });
 
             //Expected results
             Assert.IsNotNull(snurf);
-            Assert.AreEqual(snurf.Title, "ost");
+            Assert.AreEqual("ost", snurf.Title);
         }
 
-    }
-}
+        [TestMethod]
+        public void GetPropositionMethod()
+        {
+            var service = getMockService();
+
+            var snurf = service.Create(new PropositionBO()
+            {
+                Id = 1,
+                Title = "ost",
+                Description = "beskrivelse",
+                CreationDate = DateTime.MinValue,
+                EmployeeId = 2,
+                FileId = 3,
+                CustomerId = 4
+            });
+
+            var snurf2 = service.Create(new PropositionBO()
+            {
+                Id = 2,
+                Title = "ost",
+                Description = "beskrivelse",
+                CreationDate = DateTime.MinValue,
+                EmployeeId = 3,
+                FileId = 4,
+                CustomerId = 5
+            });
+
+            snurf = service.Get(1);
+
+            //Expected results
+            Assert.IsNotNull(snurf);
+            Assert.AreEqual("ost", snurf.Title);
+        }
+    }}
