b9290636652c9ed476ea9bc001774410b700c1d7
Enemark96 <je.enemark96@gmail.com>
2017-11-23 13:49:30 +0100

    test
    test

---
+++ Backend/OrderSystemForTBS/BLL/Converters/EmployeeConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BLL.BusinessObjects;
+using DAL.Entities;
+
+namespace BLL.Converters
+{
+    public class EmployeeConverter
+    {
+        public Employee Convert(EmployeeBO employee)
+        {
+            if (employee == null) { return null; }
+            {
+                return new Employee()
+                {
+                    Id = employee.Id,
+                    FirstName = employee.FirstName,
+                    LastName = employee.LastName,
+                    Username = employee.Username,
+                    Password = employee.Password,
+                    MacAddress = employee.MacAddress
+                    
+                };
+            }
+        }
+
+        public EmployeeBO Convert(Employee employee)
+        {
+            if (employee == null) { return null; }
+            return new EmployeeBO()
+            {
+                Id = employee.Id,
+                FirstName = employee.FirstName,
+                LastName = employee.LastName,
+                Username = employee.Username,
+                Password = employee.Password,
+                MacAddress = employee.MacAddress
+            };
+        }
+    }
+}
--- Backend/OrderSystemForTBS/DAL/UOW/UnitOfWork.cs
+++ Backend/OrderSystemForTBS/DAL/UOW/UnitOfWork.cs
@@ -1,28 +1,31 @@
 ﻿using DAL.Context;
 using DAL.Entities;
 using DAL.Repositories;
+using Microsoft.EntityFrameworkCore;
 
 namespace DAL.UOW
 {
     public class UnitOfWork : IUnitOfWork
     {
         public IRepository<Customer> CustomerRepository { get; internal set; }
+        public IRepository<Employee> EmployeeRepository { get; internal set; }
+        public IRepository<Proposition> PropositionRepository { get; internal set; }
 
 
-        public OrderSystemtContext context;
+        public OrderSystemContext context;
         //private static DbContextOptions<CustomerProjectContext> optionsStatic;
 
-        public UnitOfWork(DbOptions opt)
+        public UnitOfWork(OrderSystemContext context)
         {
-            context = new OrderSystemtContext();
-
+            this.context = context;
             CustomerRepository = new CustomerRepository(context);
-          
-  
-            
+            EmployeeRepository = new EmployeeRepository(context);
+            PropositionRepository = new PropositionRepository(context);
+
+
             context.Database.EnsureCreated();
         }
-
+        
         public int Complete()
         {
             //The number of objects written to the underlying database.
