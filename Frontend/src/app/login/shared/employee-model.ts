18468545fc2d4acbe3c6d73f1fa7f7ef30d8e6f1
Enemark96 <je.enemark96@gmail.com>
2017-11-24 11:27:33 +0100

    Login angular
    Login angular

---
+++ Frontend/src/app/login/shared/employee-model.ts
@@ -0,0 +1,5 @@
+export class Employee {
+  id?: number;
+  userName: string;
+  passWord: string;
+}
---
+++ Frontend/src/app/login/shared/login.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {environment} from '../../../environments/environment';
+import {Employee} from './employee-model';
+import {HttpClient} from '@angular/common/http';
+import { Http, Headers, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class LoginService {
+    public token: string;
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  login(username: string, password: string): Observable<boolean> {
+    return this.http.post('http://localhost:54210/api/login', {username: username, password: password});
+      }
+}
