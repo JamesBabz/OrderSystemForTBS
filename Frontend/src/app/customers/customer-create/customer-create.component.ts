2adafc42fb19c9ee97398be80bd8ef3428315dca
thomasmeyerh <thom953b@easv365.dk>
2017-11-27 13:04:03 +0100

    Body made for create customer
    Body made for create customer

--- Frontend/src/app/customers/customer-create/customer-create.component.ts
+++ Frontend/src/app/customers/customer-create/customer-create.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-customer-create',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerCreateComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
   }
 
+  cancel() {
+    this.router.navigateByUrl('/customers');
+  }
 }
