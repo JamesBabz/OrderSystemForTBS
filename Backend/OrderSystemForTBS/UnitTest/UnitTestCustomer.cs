79ba621a984979bad731e4fea0d1714650509c00
thomasmeyerh <thom953b@easv365.dk>
2017-11-30 21:17:48 +0100

    UNITTEST UP AT RUNNIN'

--- Backend/OrderSystemForTBS/UnitTest/UnitTestCustomer.cs
+++ Backend/OrderSystemForTBS/UnitTest/UnitTestCustomer.cs
@@ -14,7 +14,23 @@ namespace UnitTest
     [TestClass]
     public class UnitTestCustomer
     {
-
+        [TestMethod]
+        public void TestCreateCustomer()
+        {
+            this.GetMemoContext().Database.EnsureDeleted();
+            CustomerBO customer = new CustomerBO()
+                                      {
+                                          Firstname = "Bo",
+                                          Lastname = "Jensen",
+                                          Address = "Skolevej 3",
+                                          ZipCode = 4510,
+                                          City = "Dumby",
+                                          Email = "Email@mail.dk",
+                                          CVR = 12345678
+                                      };
+            this.GetService().Create(customer);
+            Assert.AreEqual(this.GetService().GetAll().Count, 1);
+        }
 
         [TestMethod]
         public void GetallCustomers()
@@ -96,6 +112,28 @@ namespace UnitTest
             GetService().Delete(customer2.Id);
             Assert.AreEqual(GetService().GetAll().Count, 1);
             Assert.AreEqual(GetService().Get(customer1.Id).Firstname, "Bo");
+
+        }
+
+        [TestMethod]
+        public void TestUpdateCustomer()
+        {
+            this.GetMemoContext().Database.EnsureDeleted();
+            CustomerBO customer1 = new CustomerBO()
+                                       {
+                                           Firstname = "Bo",
+                                           Lastname = "Jensen",
+                                           Address = "Skolevej 3",
+                                           ZipCode = 4510,
+                                           City = "Dumby",
+                                           Email = "Email@mail.dk",
+                                           CVR = 12345678
+                                       };
+            customer1 = GetService().Create(customer1);
+            Assert.AreEqual(GetService().GetAll().Count, 1);
+            customer1.Firstname = "Knud";
+            this.GetService().Update(customer1);
+            Assert.AreEqual("Knud", this.GetService().Get(1).Firstname);
 
         }
     
