085a6b0ced520d3d67980724b48190da1241ae9c
thomasmeyerh <thom953b@easv365.dk>
2017-12-07 11:15:38 +0100

    Read visit

--- Frontend/src/app/visits/visit-list/visit-list.component.ts
+++ Frontend/src/app/visits/visit-list/visit-list.component.ts
@@ -4,6 +4,8 @@ import {VisitService} from '../shared/visit.service';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Visit} from '../shared/visit.model';
 import {viewClassName} from '@angular/compiler';
+import {getDate} from 'ngx-bootstrap/bs-moment/utils/date-getters';
+import {parseTime} from 'ngx-bootstrap/timepicker/timepicker.utils';
 
 @Component({
   selector: 'app-visit-list',
@@ -12,17 +14,52 @@ import {viewClassName} from '@angular/compiler';
 })
 export class VisitListComponent implements OnInit {
 
+
   @Input()
   visits: Visit[];
   @Input()
   customer: Customer;
+  pastVisits: Visit[];
+  futureVisits: Visit[];
 
-  constructor(private visitService: VisitService,  private router: Router, private route: ActivatedRoute) { }
+
+  constructor(private visitService: VisitService, private router: Router, private route: ActivatedRoute) {
+    this.pastVisits = [];
+    this.futureVisits = [];
+  }
 
   ngOnInit() {
+
     this.route.paramMap
       .switchMap(params => this.visitService.getVisitsByCustomerId(+params.get('id')))
-      .subscribe(Visit => this.visits = Visit );
+      .subscribe(Visit => this.sortCurrentDate(this.visits = Visit));
+
+
+  }
+
+  sortCurrentDate(list: Visit[]) {
+
+    var today = new Date();
+    var currentDate = new Date(today.getTime());
+    console.log(list);
+    for (let visit of list) {
+      var date = new Date(visit.dateOfVisit);
+      var dateFromVisit = new Date(date.getTime());
+
+      if (dateFromVisit > currentDate) {
+        this.addToFutureVisits(visit);
+      } else {
+        this.addToPastVisits(visit);
+      }
+    }
+  }
+
+  addToFutureVisits(visit: Visit) {
+    this.futureVisits.push(visit);
+  }
+
+  addToPastVisits(visit: Visit) {
+    this.pastVisits.push(visit);
   }
 
 }
