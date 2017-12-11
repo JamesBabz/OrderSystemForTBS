03fecb48632b94a0c61866a1adbfbf55e83531f2
JamesBabz <web.fuhlendorff@gmail.com>
2017-12-11 14:03:15 +0100

    api
    TODO make api work

--- Frontend/src/app/customers/shared/dawa.service.ts
+++ Frontend/src/app/customers/shared/dawa.service.ts
@@ -1,15 +1,17 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {environment} from '../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs/Observable';
 
 @Injectable()
 export class DawaService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
 
-
-  getCity(zipCode: number) {
+  getCity(zipCode: number){
+    // return this.http.get('https://dawa.aws.dk/postnumre?nr=' + zipCode);
     return this.http.get(environment.ApiEndPoint + '/dawas/' + zipCode);
   }
 }
