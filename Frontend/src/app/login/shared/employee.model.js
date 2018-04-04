77064665ae51340c7c3196df3927d9ad533b65b2
Enemark96 <je.enemark96@gmail.com>
2018-04-04 13:22:06 +0200

    123

---
+++ Frontend/src/app/login/shared/employee.model.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Employee = (function () {
+    function Employee() {
+    }
+    return Employee;
+}());
+exports.Employee = Employee;
